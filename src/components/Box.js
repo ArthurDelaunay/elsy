058f4b980fcc230623ec178a256f60be744fc903
ArthurDelaunay <delaunayfredericarthur@gmail.com>
2022-07-15 17:22:59 +0200

    elsy v2 done, animations done

--- src/components/Box.js
+++ src/components/Box.js
@@ -5,9 +5,13 @@ class Box extends React.Component {
         return (
                 
             <div className="box col-sm-3 col-6">
-                <span className="material-icons" style={{fontSize : 100, color : this.props.color}}>{this.props.icon}</span>
-                <p>{this.props.value}{this.props.unit}</p>
-                {(this.props.icon !== "local_drink") && <input type="range" onChange={this.props.onChange} min={this.props.min} max={this.props.max} value={this.props.value} />}
+                {(this.props.icon === "local_drink")
+                    ? <span className={this.props.className} style={{fontSize : 100}}>{this.props.icon}</span>
+                    : <span className={this.props.className} style={{fontSize : 100, color : this.props.color}}>{this.props.icon}</span>
+                }
+                <p>{this.props.value} {this.props.unit}</p>
+                {(this.props.icon !== "local_drink" && this.props.icon !== "directions_walk") && <input type="range" onChange={this.props.onChange} min={this.props.min} max={this.props.max} value={this.props.value} />}
+                {(this.props.icon === "directions_walk") && <input type="range" onChange={this.props.onChange} min={this.props.min} max={this.props.max} value={this.props.value} step="1000" />}
             </div>
         )
     }
